931b4c042386349d08008e737ac5da4c947dd804
Farhazni Najwa <141621627+hazniy@users.noreply.github.com>
2026-02-11 11:37:23 +0000

    Create introduction to python.py

---
+++ introduction to python.py
@@ -0,0 +1,8 @@
+import numpy as np
+2 + 2
+a, b,c  = 3, 4, 5
+#select markdown in VS, write Pythagoras says that $a^2 + b^2 = c^2$  
+a, b,c  = 3, 4, 5
+a * a + b * b == c * c
+1111 ** 2 #for power of 
+np.array ([(2 ** i) % 19 for i in range(100)]) #% remainder dividing the left had operand by right hand 
