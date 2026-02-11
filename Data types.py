8acc952beac91df3f71eafc5299640c6cdec3c02
Farhazni Najwa <141621627+hazniy@users.noreply.github.com>
2026-02-11 14:09:31 +0000

    Create Data types.py

---
+++ Data types.py
@@ -0,0 +1,18 @@
+type(123.0) #float
+type(123) #int
+type(1+7j) #complex
+type(True) #bool
+type([1,2,3]) #list 
+type((1,2,3)) #tuple
+type({1,2,3}) #set 
+def f(x): 
+    return x ** 2
+type(f) #function
+
+#SET
+french = {
+    'one' : 'un', 
+    'two' : 'deux'
+    'three' : 'trois'
+}
+french['one'] #'un' 
