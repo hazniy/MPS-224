b5a60bee455f67b92f945c85b2d6b0fb919e00cb
Farhazni Najwa <141621627+hazniy@users.noreply.github.com>
2026-03-05 04:07:14 +0800

    Update Numerical solution of the Lotka-Volterra predator-prey equation.py

--- Numerical solution of the Lotka-Volterra predator-prey equation.py
+++ Numerical solution of the Lotka-Volterra predator-prey equation.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 
+#x = fish, y = shark
 def du_dt(t, u, a = 1, b = 1, c = 1, d = 1):
     """ Return du/dt for the Lotka-Volterra model. """
     x, y = u
@@ -11,10 +12,11 @@ t = np.linspace(0, 24, 4000)
 u0 = [2.9, 1.0]
 
 sol = solve_ivp(du_dt, [t[0], t[-1]], u0, t_eval = t, method = 'DOP853', atol = 1e-9, args = (1, 1, 1, 1))
-fish = sol.y[0]
-sharks = sol.y[1]
+fish = sol.y[0] #value of x 
+sharks = sol.y[1] #value of y 
 U = np.log(sharks) + np.log(fish) - sharks - fish
 
+#x and y against t 
 plt.figure(figsize = (12, 6))
 plt.plot(t, fish, color = 'red', label = 'Fish (thousands)')
 plt.plot(t, sharks, color = 'blue', label = 'Sharks (tens)')
@@ -22,6 +24,7 @@ plt.xlabel('Time (years)')
 plt.ylabel('Population')
 plt.legend(loc='upper right')
 
+#y against x, t = 0 
 plt.figure(figsize = (8, 8))
 t = np.linspace(0, 10, 1000)
 ics = np.arange(1.0, 3.0, 0.1)
