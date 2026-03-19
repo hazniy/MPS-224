89bfcf7a85a4f54916d2527729b056a985019f13
Farhazni Najwa <141621627+hazniy@users.noreply.github.com>
2026-03-19 11:28:54 +0000

    Create Assignment 1.py

---
+++ Assignment 1.py
@@ -0,0 +1,7 @@
+Part 1
+- boop to kebab : improve check 2 elements in tuple, and ensure x and y positive. 
+- kebab to boop : learn how to use reversed() function ; https://www.w3schools.com/python/ref_func_reversed.asp
+- kebab to pronk : learn how to use startswith() method, which return boolean value ; https://www.w3schools.com/python/ref_string_startswith.asp
+
+Part 2
+
